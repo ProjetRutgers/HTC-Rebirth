cdaa4dd860920bae8a7aa43a8a4e53673eb0068e
SamyShay <benachi.samy@gmail.com>
2018-05-07 12:54:52 -0400

    Portal Shine

--- HTC Rebirth/Assets/PortalCamera.cs
+++ HTC Rebirth/Assets/PortalCamera.cs
@@ -13,5 +13,11 @@ public class PortalCamera : MonoBehaviour {
 	void Update () {
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
 	}
 }
