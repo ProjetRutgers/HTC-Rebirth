47f56804ea1ab8c5ec7c989683fbb6a1a9929fbc
SamyShay <benachi.samy@gmail.com>
2018-05-08 22:58:38 +0100

    Teleport try + map PostProcess

--- HTC Rebirth/Assets/PortalTeleporter.cs
+++ HTC Rebirth/Assets/PortalTeleporter.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class PortalTeleporter : MonoBehaviour {
 
-    public Transform player;
-    public Transform receiver;
-
+	public Transform player;
     private bool playerIsOverlapping = false;
 
-	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if(playerIsOverlapping)
-        {
-            Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-            
-            if(dotProduct<0f)
-            {
-                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
-                rotationDiff += 180;
-                player.Rotate(Vector3.up, rotationDiff);
-
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-                player.position = receiver.position + positionOffset;
-
-                playerIsOverlapping = false;
-            }
-
-
+        {   
+			player.position = new Vector3 (-6f, 6f, 8f);
+			playerIsOverlapping = false;
         }
 	}
 
