cd297382c7152c94befeb10abaf0c7ae7d01d747
SamyShay <benachi.samy@gmail.com>
2018-05-09 18:45:42 +0100

    SAMY BRING UP

---
+++ HTC Rebirth/Assets/2script/PortalTeleporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+	public Transform player;
+    private bool playerIsOverlapping = false;
+
+	void Update () 
+	{
+		if(playerIsOverlapping)
+        {   
+			player.position = new Vector3 (-6f, 6f, 8f);
+			playerIsOverlapping = false;
+        }
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player" )
+        {
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
