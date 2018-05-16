62bf336219302ef27736824d27b2c7db58fef80d
SamyShay <benachi.samy@gmail.com>
2018-05-16 19:46:29 +0100

    RealSense

---
+++ HTC Rebirth/Assets/CoinRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		transform.Rotate(Vector3.up);
+	}
+}
---
+++ HTC Rebirth/Assets/PickItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickItem : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int score = 0;
+	public GUIText scoreText;
+
+
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Fire")) {
+			Destroy (other.gameObject);
+			score += 10;
+			Debug.Log ("Le score est" + score);
+
+
+		}
+
+	}
+
+
+}
